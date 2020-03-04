a617a84a0b08514d623006ab89c964fd62973538
DanielSoaresRocha <legoliga123@gmail.com>
2020-03-04 17:33:57 -0300

    start project

---
+++ frontend/LaudoLeite/src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      round
+      alt="Quasar logo"
+      spinner-color="white"
+      style="height: 128px; max-width: 128px"
+      src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s150x150/70701722_2370506326533262_8439425052318892032_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=uiGtwljSxZAAX-IhxHl&oh=fa639d06a74f9f61b326ceca10715682&oe=5E8838E2"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
