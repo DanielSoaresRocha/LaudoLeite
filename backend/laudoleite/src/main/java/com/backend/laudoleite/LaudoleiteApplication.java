7f0e55b28964489d1e817af5ed65e43cc2d9794b
WesleyLeocadio <silvawesley374@gmail.com>
2020-03-04 16:14:40 -0300

    Start project

---
+++ backend/laudoleite/src/main/java/com/backend/laudoleite/LaudoleiteApplication.java
@@ -0,0 +1,13 @@
+package com.backend.laudoleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaudoleiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaudoleiteApplication.class, args);
+	}
+
+}
