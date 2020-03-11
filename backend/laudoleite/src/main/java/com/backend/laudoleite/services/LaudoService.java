7053d27bd4e19e5b4ccd0ec07fb277531dd61413
Wesley <silvawesley374@gmail.com>
2020-03-11 11:24:25 -0300

    Criando repository e service para Laudo

---
+++ backend/laudoleite/src/main/java/com/backend/laudoleite/services/LaudoService.java
@@ -0,0 +1,24 @@
+package com.backend.laudoleite.services;
+
+import com.backend.laudoleite.domain.Laudo;
+import com.backend.laudoleite.repositories.LaudoRepository;
+import com.backend.laudoleite.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class LaudoService {
+    @Autowired
+    private LaudoRepository repo;
+
+    public Laudo find(Integer id) {
+        Optional<Laudo> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Laudo.class.getName()));
+    }
+
+
+}
