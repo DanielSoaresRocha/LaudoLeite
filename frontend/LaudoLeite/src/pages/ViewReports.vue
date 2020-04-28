70da1b6aefe266bfa05b5f194e8bbdb89ab82b5d
DanielSoaresRocha <legoliga123@gmail.com>
2020-04-28 10:50:36 -0300

    Adicionado dialog para aprovação de laudao para analise

--- frontend/LaudoLeite/src/pages/ViewReports.vue
+++ frontend/LaudoLeite/src/pages/ViewReports.vue
@@ -15,11 +15,78 @@
           :key="laudo.id"
         >
           <q-item-section>
-            {{laudo.nome}}
+            <div class="row q-gutter-md">
+              <q-btn
+                round
+                color="secondary"
+                clickable
+                v-close-popup
+                icon="done"
+                @click="showAnalyzeDialog(laudo)"
+              >
+                <q-tooltip
+                  :delay="500"
+                  :offset="[0, 10]"
+                >Colocar em análise</q-tooltip>
+              </q-btn>
+              <q-btn
+                round
+                color="red"
+                clickable
+                v-close-popup
+                icon="close"
+              >
+                <q-tooltip
+                  :delay="500"
+                  :offset="[0, 10]"
+                >Recusar análise</q-tooltip>
+              </q-btn>
+              <q-btn
+                round
+                color="secondary"
+                clickable
+                v-close-popup
+                icon="info"
+              >
+                <q-tooltip
+                  :delay="500"
+                  :offset="[0, 10]"
+                >Ver informações</q-tooltip>
+              </q-btn>
+              <q-item-label overline>
+                {{laudo.cliente}} <br><br>
+                {{laudo.analise}}
+              </q-item-label>
+            </div>
           </q-item-section>
         </q-item>
       </q-list>
     </div>
+    <q-dialog v-model="showAnalyze">
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="col">
+            <center>
+              <h5>Deseja confirmar a aprovação do laudo de {{laudo.cliente}} para análise?</h5>
+            </center>
+            <br>
+            <div class="q-pa-md q-gutter-md row justify-end">
+              <q-btn
+                label="cancelar"
+                color="red"
+                @click="showAnalyze = false"
+                clickable
+              />
+              <q-btn
+                @click="approveReservation()"
+                label="confirmar"
+                color="green"
+              />
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -28,22 +95,25 @@ import api from '../services/api'
 
 export default {
   name: 'ViewReports',
-  created () {
-    this.getReports()
+  async created () {
+    this.laudosPendentes = await this.getReports()
   },
   data () {
     return {
-      laudosPendentes: [
-        { id: '1', nome: 'laudo 1', tipo: 'microbiológico' },
-        { id: '2', nome: 'laudo 2', tipo: 'físico químico' },
-        { id: '3', nome: 'laudo 3', tipo: 'microbiológico' }
-      ]
+      laudosPendentes: [],
+      showApproved: false,
+      showAnalyze: false,
+      laudo: {}
     }
   },
   methods: {
     async getReports () {
-      const response = await api.get('/laudos/1', {})
-      console.log(response.data)
+      const response = await api.get('/laudos', {})
+      return response.data
+    },
+    showAnalyzeDialog (laudo) {
+      this.showAnalyze = true
+      this.laudo = laudo
     }
   }
 }
