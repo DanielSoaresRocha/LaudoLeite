bfef850c3940701d98e6ec55253927fb34455aa4
DanielSoaresRocha <legoliga123@gmail.com>
2020-04-30 22:33:30 -0300

    Mostrando todas as informações na ná analise microbiológica

--- frontend/LaudoLeite/src/pages/Report.vue
+++ frontend/LaudoLeite/src/pages/Report.vue
@@ -30,10 +30,96 @@
           </q-card-section>
         </q-card>
       </div>
-      <div class="col-5 q-pa-xs">
+      <div class="col-6 q-pa-xs">
         <q-card>
           <q-card-section style="background-color: Silver">
             <b>Produto:</b> <br> {{laudo.produto}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Análise solicitada:</b> <br> {{laudo.analise_solicitada}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="text-h6 col-12 q-pa-md">
+        Dados do produto:
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Nº SIF/IDIARN/SIM:</b> <br> {{laudo.sif_idiarn_sim}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Tipo de amostra:</b> <br> {{tipoApostra()}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Data da produção: </b> <br> {{laudo.data_producao}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Tipo da validade:</b> <br> {{laudo.data_validacao}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="text-h6 col-12 q-pa-md">
+        Condições da amostra no recebimento:
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Temperatura : </b> <br> {{laudo.temperatura}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Estado:</b> <br> Resfriado
+            <!-- Falta retornar do banco {{laudo.estado}} -->
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Ordem de serviço:</b> <br> {{laudo.ordem_servico}}
+            <!-- Falta retornar do banco {{laudo.estado}} -->
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Respondável pelo recebimento:</b> <br> {{laudo.responsavel_recebimento}}
+            <!-- Falta retornar do banco {{laudo.estado}} -->
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>Data do recebimento: </b> <br> {{laudo.data_recebimento}}
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-card>
+          <q-card-section style="background-color: Silver">
+            <b>hora do recebimento:</b> <br> {{laudo.hora_recebimento}}
           </q-card-section>
         </q-card>
       </div>
@@ -54,6 +140,15 @@ export default {
   data () {
     return {
       laudo: {}
+    }
+  },
+  methods: {
+    tipoApostra () {
+      if (this.laudo.individual) {
+        return 'individual'
+      } else {
+        return 'Não individual'
+      }
     }
   }
 }
